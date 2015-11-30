1d1594c6b87c4d58cc8949a6e256034e532552d6
Erik Sälgström Peterson <erik@swedenunlimited.com>
2015-11-29 20:38:32 -0500

    WIP

--- board.js
+++ board.js
@@ -3,7 +3,8 @@
 const EventEmitter = require('events');
 const shuffle = require('shuffle-array');
 const NUMBERS = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
-const Map = require('./map');
+const PLACEMENT_REGEX = /^random|classic$/;
+const HexMap = require('./hexmap');
 
 class Board extends EventEmitter {
 
@@ -27,12 +28,46 @@ class Board extends EventEmitter {
 	}
 
 	__setMap(){
-		this.map = new Map(this.tiles);
+		this.hexmap = new HexMap(this.tiles);
 	}
 
 	__getNumber(type){
 		this.numbers = this.numbers || shuffle(NUMBERS);
 		return type === 'desert' ? null : this.numbers.pop();
+	}
+
+	static validateOptions(options){
+        let message = '';
+        let invalid = Object.keys(options)
+                    .some(function(key){
+                        let method = `validate_${key.toUppercase(0)}`;
+                        if(!method in Board){
+                            message = `${key}: Invalid configuration key`;
+                            return true;
+                        } 
+                        let optValid = Board[method](options));
+                        if(optValid){
+                            message = optValid;
+                            return true;
+                        }
+                        return false;
+                    });
+
+        return invalid ? message : false;
+
+
+        
+        let landsInvalid = ('lands' in board) ? validate_lands(board.lands) : false;
+        if(landsInvalid) return landsInvalid;
+	}
+
+	static validate_placement(options){
+        let isarray = Array.isArray(options.placement);
+        let istring = (typeof options.placement === 'string');
+        if(!isarray && !istring) return "placement: Must be of type Array or String";
+        if(isarray) return Board.validate_serialized_tiles(options);
+        if(!PLACEMENT_REGEX.test(options.placement)) return `placement: '${options.placement}' is not a valid setting`;
+		return false;
 	}
 }
 
--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const EventEmitter = require('events');
 const shuffle = require('array-shuffle');
 const Board = require('./board.js');
 
-const defaults = {
+const smallDefaults = {
     players: {
         count: 3,
         placement: 'random'
@@ -23,14 +23,37 @@ const defaults = {
     }
 };
 
+const bigDefaults = {
+    players: {
+        count: 5,
+        placement: 'random'
+    },
+    pointsToWin: 10,
+    board: {
+        lands: {
+            desert: 2,
+            hills: 5,
+            pastures: 6,
+            fields: 6, 
+            mountains: 5,
+            forests: 6
+        },
+        placement: 'random'
+    }
+};
+
 let turn = 0;
 const players = [];
 
 class Game extends EventEmitter {
+
     constructor(options){
+        let validation = Game.validateOptions(options);
+        if(validation) return this.__emitError(validation);
+
         super();
         this.opts = Object.assign({}, defaults, options || {});
-        this.__constructPromises();
+        this.promise = this.__constructPromises();
     }
     
     get players(){
@@ -46,6 +69,7 @@ class Game extends EventEmitter {
         this.players.push(new Player(options));
         if(this.players.length === this.opts.players.count) this.__playersReady();
     }
+
     __constructPromises(){
         let self = this;
         let boardPromise = new Promise(function(resolve, reject){
@@ -59,14 +83,17 @@ class Game extends EventEmitter {
         let playerPromise = new Promise(function(resolve){
             self.on('players:ready', resolve);
         });
-        Promise.all(boardPromise, playerPromse)
-            .then(()=>this.emit('game:ready', this));
+
+        return Promise.all(boardPromise, playerPromse)
+            .then(()=>this.emit('game:ready', this),
+                  (err)=>this.emit('error', err));
     }
 
     __buildBoard(){
         this.board = new Board(this.opts.board);
         this.emit('board:ready', this.board);
     }
+
     __playersReady(){
         let placement = this.opts.players.placement;
         
@@ -78,10 +105,50 @@ class Game extends EventEmitter {
 
         this.emit('players:ready', this.players);
     }
+
     __emitError(message){
         message = message || 'An error occured';
         this.emit('error', new Error(message));
     }
+
+    static validateOptions(options){
+        let message = '';
+        let invalid = Object.keys(options)
+                    .some(function(key){
+                        let method = `validate_${key.toUppercase(0)}`;
+                        if(!method in Game){
+                            message = `Invalid configuration key: ${key}`;
+                            return true;
+                        } 
+                        let optValid = Game[method](options[key]));
+                        if(optValid){
+                            message = optValid;
+                            return true;
+                        }
+                        return false;
+                    });
+        return invalid ? message : false;
+    }
+
+    static validate_players(players){
+        if(players.count > 6) return 'options.players.count: Too many players';
+        if(players.count < 3) return 'options.players.count: Too few players';
+
+        let placementRegex = /random|join|reversejoin/;
+        if(!players.placement.call && !placementRegex.test(players.placement)) return "options.players.placement: Must be 'random', 'join', 'reversejoin' or an instance of Function";
+
+        return false;
+    }
+
+    static validate_pointsToWin(pointsToWin){
+        return pointsToWin < 2 ? 'options.players.pointsToWin: Must be greater than 2' : false;
+    }
+
+    static validate_board(board){
+        let boardInvalid = Board.validate(board);
+        return boardInvaid ? `options.board.${boardInvalid}` : false;
+    }
+
 }
 
 module.exports = Game;
